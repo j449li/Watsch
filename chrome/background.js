72c6e36cc6c82ce73558cd5fee8fb58b04eb1513
George Li <jiali@sysomos.com>
2015-04-22 18:38:37 -0400

    - indentation

--- chrome/background.js
+++ chrome/background.js
@@ -1,17 +1,17 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.showPageAction) {
-    	chrome.tabs.query(
-	        {currentWindow: true, active : true},
-	        function(tabArray) {
-	    		chrome.pageAction.show(tabArray[0].id);
-	    	}
-	    );
+        chrome.tabs.query(
+            {currentWindow: true, active : true},
+            function(tabArray) {
+            	chrome.pageAction.show(tabArray[0].id);
+            }
+        );
     } else if (request.hidePageAction) {
         chrome.tabs.query(
-	        {currentWindow: true, active : true},
-	        function(tabArray) {
-	    		chrome.pageAction.hide(tabArray[0].id);
-	    	}
-	    );
+        	{currentWindow: true, active : true},
+            function(tabArray) {
+        	    chrome.pageAction.hide(tabArray[0].id);
+	        }
+        );
     }
 });
--- chrome/content.js
+++ chrome/content.js
@@ -6,55 +6,54 @@ function killPolling() {
 }
 
 function setPageActionVisibility(visible) {
-	if (isPageActionVisible && !visible) {
-		console.log("pageAction is hidden");
-		chrome.runtime.sendMessage({hidePageAction: true});
-	} else if (!isPageActionVisible && visible) {
-		console.log("pageAction is visible");
-		chrome.runtime.sendMessage({showPageAction: true});
-	}
-
-	isPageActionVisible = visible;
+    if (isPageActionVisible && !visible) {
+    	console.log("pageAction is hidden");
+    	chrome.runtime.sendMessage({hidePageAction: true});
+    } else if (!isPageActionVisible && visible) {
+    	console.log("pageAction is visible");
+    	chrome.runtime.sendMessage({showPageAction: true});
+    }
+    isPageActionVisible = visible;
 }
 
 function getActionState() {
-	var titleEle = document.getElementById("DERIVED_REGFRM1_SS_TRANSACT_TITLE");
-	var listRadioBtn = document.getElementById("DERIVED_REGFRM1_SSR_SCHED_FORMAT$258$");
+    var titleEle = document.getElementById("DERIVED_REGFRM1_SS_TRANSACT_TITLE");
+    var listRadioBtn = document.getElementById("DERIVED_REGFRM1_SSR_SCHED_FORMAT$258$");
 
-	if (titleEle != null && listRadioBtn != null
-		    && titleEle.textContent == "My Class Schedule"
-		    && listRadioBtn.checked) {
-		return ActionStates.SHOW;
+    if (titleEle != null && listRadioBtn != null
+            && titleEle.textContent == "My Class Schedule"
+            && listRadioBtn.checked) {
+        return ActionStates.SHOW;
     } else if (titleEle != null 
-		    && titleEle.textContent == "My Class Schedule") {
-	    return ActionStates.HIDE;
+            && titleEle.textContent == "My Class Schedule") {
+        return ActionStates.HIDE;
     } else {
-    	return ActionStates.GONE;
+        return ActionStates.GONE;
     }
 
 }
 
 function pollState() {
-	switch(getActionState()) {
-		case ActionStates.SHOW:
-		    console.log("SHOW");
-			setPageActionVisibility(true);
-		    break;
-		case ActionStates.HIDE:
-		    console.log("HIDE");
-		    setPageActionVisibility(false);
-		    break;
-		case ActionStates.GONE:
-		    console.log("GONE");
-		    killPolling();
-		    break;
-	}
+    switch(getActionState()) {
+        case ActionStates.SHOW:
+            console.log("SHOW");
+            setPageActionVisibility(true);
+            break;
+        case ActionStates.HIDE:
+            console.log("HIDE");
+            setPageActionVisibility(false);
+            break;
+        case ActionStates.GONE:
+            console.log("GONE");
+            killPolling();
+            break;
+    }
 }
 
 ActionStates = {
-	SHOW : 1,
-	HIDE : 2,
-	GONE : 3
+    SHOW : 1,
+    HIDE : 2,
+    GONE : 3
 }
 
 var polling;
@@ -63,5 +62,5 @@ var isPageActionVisible = false;
 // Always hide it onload
 chrome.runtime.sendMessage({hidePageAction: true});
 if (getActionState() != ActionStates.GONE) {
-	polling = setInterval(pollState, 500);
+    polling = setInterval(pollState, 500);
 }
